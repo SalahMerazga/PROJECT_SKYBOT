f60c9d3d7882219697f92cf10de791393632d333
Salah <merazga.salah.el.dine@gmail.com>
2024-09-23 13:54:10 +0200

    LED Clignotante

---
+++ src/main_GPIO.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+int led = 16;
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, HIGH);
+  delay(500);
+}
